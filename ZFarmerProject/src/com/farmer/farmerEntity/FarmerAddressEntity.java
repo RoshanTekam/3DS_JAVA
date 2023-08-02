e6729b36fc84ea5e27c63468aa5e4e6dd1697e1d
RoshanTekam <140386337+RoshanTekam@users.noreply.github.com>
2023-08-02 17:07:50 +0530

    Add files via upload

---
+++ ZFarmerProject/src/com/farmer/farmerEntity/FarmerAddressEntity.java
@@ -0,0 +1,14 @@
+package com.farmerProject.entities.farmerEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class FarmerAddressEntity {
+	// Address details
+	private String city;
+	private String state;
+	private String pinCode;
+
+}
